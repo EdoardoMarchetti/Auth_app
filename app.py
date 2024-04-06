d440276af3cf7817edbffbec8ca6e8c213616689
EdoardoMarchetti <62024316+EdoardoMarchetti@users.noreply.github.com>
2024-04-06 19:17:32 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,47 @@
+import yaml
+import streamlit as st
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
+from streamlit_authenticator.utilities.exceptions import (CredentialsError,
+                                                          ForgotError,
+                                                          LoginError,
+                                                          RegisterError,
+                                                          ResetError,
+                                                          UpdateError) 
+
+# Loading config file
+with open('./config.yaml', 'r', encoding='utf-8') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+# Creating the authenticator object
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
+
+print('-------------------------')
+print('Chiavi sessione')
+for k in st.session_state.keys():
+    print(k)
+print('-------------------------')
+
+
+# Creating a login widget
+try:
+    authenticator.login()
+except LoginError as e:
+    st.error(e)
+
+
+
+if st.session_state["authentication_status"]:
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
